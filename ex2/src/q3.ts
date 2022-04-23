2ef32e6850283ee276f4811855c53f5c4d1687d7
tal <talpar@post.bgu.ac.il>
2022-04-23 19:26:05 +0300

    started 3.b

--- ex2/src/q3.ts
+++ ex2/src/q3.ts
@@ -1,5 +1,7 @@
-import {  Exp, Program } from "./L31-ast";
-import { Result, makeFailure } from "../shared/result";
+import {  Exp, Program, isProgram, isExp , isDefineExp, isNumExp, isBoolExp, isPrimOp, isVarRef, isAppExp, isProcExp, isLetExp, isIfExp, isLitExp, Binding, CExp, makeLetExp, makeBinding, makeProcExp, makeIfExp, makeAppExp, makeProgram, makeDefineExp, isLetPlusExp } from "./L31-ast";
+import { Result, makeFailure, makeOk, safe2, mapResult, bind } from "../shared/result";
+import { map, zipWith } from "ramda";
+
 
 
 /*
@@ -8,4 +10,31 @@ Signature: l31ToL3(l31AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    makeFailure("TODO");
+    isProgram(exp) ? bind(mapResult(L31ExpToL3, exp.exps), (exps: Exp[]) => makeOk(makeProgram(exps))) :
+    isExp(exp) ? L31ExpToL3(exp) :
+    makeFailure("Wrong input");
+
+export const L31ExpToL3 = (exp: Exp): Result<Exp> =>
+    isDefineExp(exp) ? bind(L31CExpToL3(exp.val), (val: CExp) => makeOk(makeDefineExp(exp.var, val))) :
+    L31CExpToL3(exp);
+
+
+export const L31CExpToL3 = (exp: CExp): Result<CExp> =>
+    isNumExp(exp) ? makeOk(exp) :
+    isBoolExp(exp) ? makeOk(exp) :
+    isPrimOp(exp) ? makeOk(exp) :
+    isVarRef(exp) ? makeOk(exp) :
+    isAppExp(exp) ? safe2((rator: CExp, rands: CExp[]) => makeOk(makeAppExp(rator, rands)))
+                        (L31CExpToL3(exp.rator), mapResult(L31CExpToL3, exp.rands)) :
+    isIfExp(exp) ? makeOk((makeIfExp((exp.test),(exp.then),(exp.alt)))):
+    isProcExp(exp) ? bind(mapResult(L31CExpToL3, exp.body), (body: CExp[]) => makeOk(makeProcExp(exp.args, body))) :
+    isLetExp(exp) ? safe2((vals : CExp[], body: CExp[]) => makeOk(makeLetExp(zipWith(makeBinding,map(binding => binding.var.var, exp.bindings), vals), body)))
+               (mapResult((binding : Binding ) => L31CExpToL3(binding.val), exp.bindings), mapResult(L31CExpToL3,exp.body)) :
+    isLetPlusExp(exp) ? 
+    isLitExp(exp) ? makeOk(exp) :
+    makeFailure(`Unexpected CExp: ${exp.tag}`);
+
+
+
+    
+
