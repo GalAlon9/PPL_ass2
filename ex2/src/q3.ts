7be0833272f3f311c3b7fb27f14729e0d9cd4773
tal <talpar@post.bgu.ac.il>
2022-04-25 21:07:22 +0300

    done 3.b

--- ex2/src/q3.ts
+++ ex2/src/q3.ts
@@ -1,21 +1,28 @@
-import { map } from "ramda";
-import { AppExp, CExp, Exp, LetExp, Program }  from "./L31-ast";
-import { isAppExp, isAtomicExp, isCExp, isDefineExp, isExp, isIfExp, isLetExp, isLitExp,
-         isProcExp, isProgram, makeAppExp, makeDefineExp, makeIfExp, makeProcExp, makeProgram }  from "./L31-ast";
+import { map, zipWith } from "ramda";
+import { AppExp, CExp, Exp, LetExp, makeLetExp, makeLetPlusExp, Program }  from "./L31-ast";
+import {isVarRef, isPrimOp, isBoolExp,isNumExp,isAppExp, isAtomicExp, isCExp, isDefineExp, isExp, isIfExp, isLetExp, isLitExp,
+         isProcExp, isProgram, makeAppExp, makeDefineExp, makeIfExp, makeProcExp, makeProgram,makeBinding,Binding,isLetPlusExp, LetPlusExp }  from "./L31-ast";
 import { Result, bind, makeFailure, makeOk, mapResult, safe2, mapv } from "../shared/result";
+import { makeBox } from "../shared/box";
 
 
-/*
-Purpose: rewrite a single LetExp as a lambda-application form
-Signature: rewriteLet(cexp)
-Type: [LetExp => AppExp]
-*/
-const rewriteLet = (e: LetExp): AppExp => {
+
+export const rewriteLetPlusCExp = (e: LetPlusExp): Result<CExp> => {
     const vars = map((b) => b.var, e.bindings);
     const vals = map((b) => b.val, e.bindings);
-    return makeAppExp(
-            makeProcExp(vars, e.body),
-            vals);
+    //if binding == 1 --> return makelet
+    // makeLetExp(binding[0], rewrite(makeletplus(slice(binding), body)))
+    const exp = rewriteLetPlus(e);
+    return safe2((bindings: CExp[], body: CExp[]) => makeOk(makeLetExp(zipWith(makeBinding,map(binding => binding.var.var, exp.bindings),bindings),body)))
+                            (mapResult((binding : Binding ) => rewriteAllLetPlusCExp(binding.val), exp.bindings),
+                            mapResult(rewriteAllLetPlusCExp, exp.body))
+}
+
+export const rewriteLetPlus = (e: LetPlusExp): LetExp => {
+    if (e.bindings.length == 1) {
+        return makeLetExp(e.bindings, e.body);
+    }
+    return makeLetExp(e.bindings.slice(0,1), makeBox(makeLetPlusExp(e.bindings.slice(1),e.body)));
 }
 
 
@@ -35,6 +42,10 @@ const rewriteAllLetPlusExp = (exp: Exp): Result<Exp> =>
 
 const rewriteAllLetPlusCExp = (exp: CExp): Result<CExp>=>
     isAtomicExp(exp) ? makeOk(exp) :
+    isNumExp(exp) ? makeOk(exp) :
+    isBoolExp(exp) ? makeOk(exp) :
+    isPrimOp(exp) ? makeOk(exp) :
+    isVarRef(exp) ? makeOk(exp) :
     isLitExp(exp) ? makeOk(exp) :
     isIfExp(exp) ? three_binds((test: CExp, then: CExp, alt: CExp) => makeOk( makeIfExp(test,then,alt)))
                              (rewriteAllLetPlusCExp(exp.test),
@@ -43,7 +54,10 @@ const rewriteAllLetPlusCExp = (exp: CExp): Result<CExp>=>
     isAppExp(exp) ? safe2((rator: CExp, rands: CExp[])=> makeOk(makeAppExp(exp.rator,exp.rands)))
                             (rewriteAllLetPlusCExp(exp.rator), mapResult(rewriteAllLetPlusCExp, exp.rands)):                      
     isProcExp(exp) ? bind(mapResult(rewriteAllLetPlusCExp, exp.body), (body: CExp[]) => makeOk(makeProcExp(exp.args,body))):
-    isLetExp(exp) ? rewriteAllLetPlusCExp(rewriteLet(exp)) :
+    isLetExp(exp) ? safe2((bindings: CExp[], body: CExp[]) => makeOk(makeLetExp(zipWith(makeBinding,map(binding => binding.var.var, exp.bindings),bindings),body)))
+                            (mapResult((binding : Binding ) => rewriteAllLetPlusCExp(binding.val), exp.bindings),
+                            mapResult(rewriteAllLetPlusCExp, exp.body)):
+    isLetPlusExp(exp) ? rewriteLetPlusCExp(exp) :
      makeFailure("error");
 
 
