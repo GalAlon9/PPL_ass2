f8da04adc6ccafba0ae68ee7bfa4d866d4bcb7e9
tal <talpar@post.bgu.ac.il>
2022-04-25 19:49:06 +0300

    WIP 3.b

--- ex2/src/q3.ts
+++ ex2/src/q3.ts
@@ -1,7 +1,22 @@
-import {  Exp, Program, isProgram, isExp , isDefineExp, isNumExp, isBoolExp, isPrimOp, isVarRef, isAppExp, isProcExp, isLetExp, isIfExp, isLitExp, Binding, CExp, makeLetExp, makeBinding, makeProcExp, makeIfExp, makeAppExp, makeProgram, makeDefineExp, isLetPlusExp } from "./L31-ast";
-import { Result, makeFailure, makeOk, safe2, mapResult, bind } from "../shared/result";
-import { map, zipWith } from "ramda";
+import { map } from "ramda";
+import { AppExp, CExp, Exp, LetExp, Program }  from "./L31-ast";
+import { isAppExp, isAtomicExp, isCExp, isDefineExp, isExp, isIfExp, isLetExp, isLitExp,
+         isProcExp, isProgram, makeAppExp, makeDefineExp, makeIfExp, makeProcExp, makeProgram }  from "./L31-ast";
+import { Result, bind, makeFailure, makeOk, mapResult, safe2, mapv } from "../shared/result";
 
+
+/*
+Purpose: rewrite a single LetExp as a lambda-application form
+Signature: rewriteLet(cexp)
+Type: [LetExp => AppExp]
+*/
+const rewriteLet = (e: LetExp): AppExp => {
+    const vars = map((b) => b.var, e.bindings);
+    const vals = map((b) => b.val, e.bindings);
+    return makeAppExp(
+            makeProcExp(vars, e.body),
+            vals);
+}
 
 
 /*
@@ -10,29 +25,35 @@ Signature: l31ToL3(l31AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    isProgram(exp) ? bind(mapResult(L31ExpToL3, exp.exps), (exps: Exp[]) => makeOk(makeProgram(exps))) :
-    isExp(exp) ? L31ExpToL3(exp) :
-    makeFailure("Wrong input");
+isProgram(exp) ? bind(mapResult(rewriteAllLetPlusExp, exp.exps),(exp: Exp[]) => makeOk(makeProgram(exp))) :
+isExp(exp) ? rewriteAllLetPlusExp(exp) :
+makeFailure("Failure");
 
-export const L31ExpToL3 = (exp: Exp): Result<Exp> =>
-    isDefineExp(exp) ? bind(L31CExpToL3(exp.val), (val: CExp) => makeOk(makeDefineExp(exp.var, val))) :
-    L31CExpToL3(exp);
+const rewriteAllLetPlusExp = (exp: Exp): Result<Exp> =>
+    isDefineExp(exp) ? bind(rewriteAllLetPlusCExp(exp.val), (val: CExp) => makeOk(makeDefineExp(exp.var, val))):
+    rewriteAllLetPlusCExp(exp);
 
-
-export const L31CExpToL3 = (exp: CExp): Result<CExp> =>
-    isNumExp(exp) ? makeOk(exp) :
-    isBoolExp(exp) ? makeOk(exp) :
-    isPrimOp(exp) ? makeOk(exp) :
-    isVarRef(exp) ? makeOk(exp) :
-    isAppExp(exp) ? safe2((rator: CExp, rands: CExp[]) => makeOk(makeAppExp(rator, rands)))
-                        (L31CExpToL3(exp.rator), mapResult(L31CExpToL3, exp.rands)) :
-    isIfExp(exp) ? makeOk((makeIfExp((exp.test),(exp.then),(exp.alt)))):
-    isProcExp(exp) ? bind(mapResult(L31CExpToL3, exp.body), (body: CExp[]) => makeOk(makeProcExp(exp.args, body))) :
-    isLetExp(exp) ? safe2((vals : CExp[], body: CExp[]) => makeOk(makeLetExp(zipWith(makeBinding,map(binding => binding.var.var, exp.bindings), vals), body)))
-               (mapResult((binding : Binding ) => L31CExpToL3(binding.val), exp.bindings), mapResult(L31CExpToL3,exp.body)) :
-    isLetPlusExp(exp) ? 
+const rewriteAllLetPlusCExp = (exp: CExp): Result<CExp>=>
+    isAtomicExp(exp) ? makeOk(exp) :
     isLitExp(exp) ? makeOk(exp) :
-    makeFailure(`Unexpected CExp: ${exp.tag}`);
+    isIfExp(exp) ? three_binds((test: CExp, then: CExp, alt: CExp) => makeOk( makeIfExp(test,then,alt)))
+                             (rewriteAllLetPlusCExp(exp.test),
+                             rewriteAllLetPlusCExp(exp.then),
+                             rewriteAllLetPlusCExp(exp.alt)):
+    isAppExp(exp) ? safe2((rator: CExp, rands: CExp[])=> makeOk(makeAppExp(exp.rator,exp.rands)))
+                            (rewriteAllLetPlusCExp(exp.rator), mapResult(rewriteAllLetPlusCExp, exp.rands)):                      
+    isProcExp(exp) ? bind(mapResult(rewriteAllLetPlusCExp, exp.body), (body: CExp[]) => makeOk(makeProcExp(exp.args,body))):
+    isLetExp(exp) ? rewriteAllLetPlusCExp(rewriteLet(exp)) :
+     makeFailure("error");
+
+
+
+
+export const three_binds = <T1, T2, T3, T4>(f: (x: T1, y: T2, z: T3) => Result<T4>): (a: Result<T1>, b: Result<T2>, c: Result<T3>) => Result<T4> =>
+    (a: Result<T1>, b: Result<T2>, c: Result<T3>) =>
+        bind(a, (x: T1) => bind(b, (y: T2) => bind(c, (z: T3) => f(x, y, z))));
+
+
 
 
 
